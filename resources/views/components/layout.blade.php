62c0e349f1954201fd02dcdd4b7b701da8e64c38
deddythok <deddy.donchild@gmail.com>
2025-01-02 14:22:35 +0700

    adding css & tailwind

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ninja Network | Home</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <header>
+        <nav>
+            <h1>Ninja Network</h1>
+            <a href="/ninjas">All ninjas</a>
+            <a href="/ninjas/create">Create new Ninjas</a>
+        </nav>
+    </header>
+
+    <main class="container">
+        {{ $slot }}
+    </main>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ninja network</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    @vite('resources/css/app.css')
+</head>
+<body class="text-center px-8 py-12">
+    <h1>Welcome to the Ninja Network</h1>
+    <p>Click the button below to view the list of ninjas.</p>
+
+    <a href="/ninjas" class="btn mt-4 inline-block">
+        Find Ninjas!
+    </a>
+</body>
+</html>
