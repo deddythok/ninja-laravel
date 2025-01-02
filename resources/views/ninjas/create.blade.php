113a0b2457860a4349154bf99f3b4d9a0bd33167
nd3d <deddy.donchild@gmail.com>
2025-01-02 23:01:08 +0700

    adding old helper

--- resources/views/ninjas/create.blade.php
+++ resources/views/ninjas/create.blade.php
@@ -19,6 +19,7 @@
           type="number" 
           id="skill" 
           name="skill" 
+          value="{{ old('skill') }}"
           required
         >
       
@@ -29,14 +30,14 @@
           id="bio" 
           name="bio" 
           required
-        ></textarea>
+        >{{old('bio')}}</textarea>
       
         <!-- select a dojo -->
         <label for="dojo_id">Dojo:</label>
         <select id="dojo_id" name="dojo_id" required>
           <option value="" disabled selected>Select a dojo</option>
           @foreach ($dojos as $dojo)
-              <option value="{{ $dojo->id }}">
+              <option value="{{ $dojo->id }}" {{ old('dojo_id') == $dojo->id ? 'selected' : '' }}>
                 {{ $dojo->name }}
               </option>
           @endforeach
@@ -45,6 +46,12 @@
         <button type="submit" class="btn mt-4">Create Ninja</button>
       
         <!-- validation errors -->
-        
+        @if ($errors->any())
+            <ul class="px-4 py-2 bg-red-100">
+              @foreach ($errors->all() as $error)
+                <li class="my-2 text-red-500">{{ $error }}</li>
+              @endforeach
+            </ul>
+        @endif
       </form>
 </x-layout>
