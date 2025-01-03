e96fa419c8997b67c3fe71c11e050ac9c4042376
deddythok <deddy.donchild@gmail.com>
2025-01-03 17:04:27 +0700

    add edit functionality

--- app/Http/Controllers/NinjaController.php
+++ app/Http/Controllers/NinjaController.php
@@ -37,4 +37,20 @@ class NinjaController extends Controller
         $ninja->delete();
         return redirect()->route('ninjas.index')->with('success', 'Ninja deleted successfully');
     }
+    public function edit(Ninja $ninja) {
+        $dojos = Dojo::all();
+        return view('ninjas.edit', ['ninja' => $ninja, 'dojos' => $dojos])->with('success', 'Ninja updated successfully');
+    }
+    public function update(Request $request, Ninja $ninja) {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'skill' => 'required|integer|min:0|max:100',
+            'bio' => 'required|string|min:20|max:1000',
+            'dojo_id' => 'required|exists:dojos,id'
+        ]);
+
+        $ninja->update($validated);
+
+        return redirect()->route('ninjas.show', $ninja)->with('success', 'Ninja updated successfully');
+    }
 }
